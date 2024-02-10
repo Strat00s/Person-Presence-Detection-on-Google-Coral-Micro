eba6e0cfc0a77187c0790f6f626dd10c0b448aad
Stratos <l.bastyr@seznam.cz>
2024-02-10 19:13:40 +0100

    first build and fixed directory

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 cd $SCRIPT_DIR/..
+
+# clone the coral micro repo
 git clone --recurse-submodules -j8 https://github.com/google-coral/coralmicro
 
 # copy the source
@@ -13,5 +14,8 @@ echo "add_subdirectory(my_project)" >> coralmicro/apps/CMakeLists.txt
 # set working hidapi version
 sed -i 's/^hidapi==.*$/hidapi==0.11.2/' coralmicro/scripts/requirements.txt
 
-# run the setup
-setup.sh
+# run the coral micro setup (requires root access for apt update)
+coralmicro/setup.sh
+
+# run initial build
+coralmicro/build.sh
