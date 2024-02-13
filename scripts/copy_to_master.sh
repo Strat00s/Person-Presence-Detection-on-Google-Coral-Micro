8db1de1cb05faa142df42a733bc7be442a97fda2
Stratos <l.bastyr@seznam.cz>
2024-02-13 18:12:02 +0100

    cleanup

--- scripts/copy_to_master.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/..
-cp -r coralmicro/apps/my_project/* src/
-cp -r coralmicro/apps/camera_testing/* testing/camera_testing
